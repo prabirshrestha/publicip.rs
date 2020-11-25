c631d0afa26fef4be01dcd26d8e3f14e542027e2
Prabir Shrestha <mail@prabir.me>
2020-11-25 01:59:01 +0000

    use Box dyn error

--- src/bin/publicip.rs
+++ src/bin/publicip.rs
@@ -1,9 +1,7 @@
 extern crate publicip;
 
-use anyhow::Result;
-
 #[async_std::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     println!("IP v4: {}", publicip::v4().await?);
     println!("IP v6: {}", publicip::v6().await?);
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
-use anyhow::Result;
-
-pub async fn v4() -> Result<String> {
-    // let mut res = surf::get("https://api.ipify.org/").await?;
-    // Ok(res.body_string().await?)
-    Ok(String::from(""))
+pub async fn v4() -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+    let mut res = surf::get("https://api.ipify.org/").await?;
+    Ok(res.body_string().await?)
 }
 
-pub async fn v6() -> Result<String> {
-    // let mut res = surf::get("https://api64.ipify.org/").await?;
-    // Ok(res.body_string().await?)
-    Ok(String::from(""))
+pub async fn v6() -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+    let mut res = surf::get("https://api64.ipify.org/").await?;
+    Ok(res.body_string().await?)
 }
